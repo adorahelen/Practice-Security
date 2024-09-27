2ebafd346d586a89f78157e754b4f4ef447654eb
adorahelen <adorahelenmin@gmail.com>
2024-09-27 12:14:07 +0900

    기본 세팅(성공 후 핸들러)

---
+++ src/main/java/kdt/hackathon/practicesecurity/config/ouath/OAuth2SuccessHandler.java
@@ -0,0 +1,84 @@
+package kdt.hackathon.practicesecurity.config.ouath;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kdt.hackathon.practicesecurity.OAuth2.CookieUtil;
+import kdt.hackathon.practicesecurity.config.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import kdt.hackathon.practicesecurity.config.jwt.TokenProvider;
+import kdt.hackathon.practicesecurity.entity.RefreshToken;
+import kdt.hackathon.practicesecurity.entity.User;
+import kdt.hackathon.practicesecurity.repository.RefreshTokenRepository;
+import kdt.hackathon.practicesecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
+    public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
+    public static final String REDIRECT_PATH = "/service"; // articles -> service
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestBasedOnCookieRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Optional<User> user = userRepository.findByEmail((String) oAuth2User.getAttributes().get("email"));
+
+
+        // logic 리프레쉬 토큰 생성 -> 저장 -> 쿠키에 저장
+        String refreshToken = tokenProvider.generateToken(user.get(), REFRESH_TOKEN_DURATION);
+    }
+
+
+    // 1. 생성된 리프레쉬 토큰을 전달받아 데이터베이스에 저장
+    private void saveRefreshToken(String userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.updateToken(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
+    }
+
+    // 2. 생성된 리프레쉬 토큰을 쿠키에 저장
+    private void addRefreshTokenCookie(HttpServletRequest request,
+                                       HttpServletResponse response,
+                                       String refreshToken) {
+        int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.toSeconds();
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
+    }
+
+    // 3. 인증 관련 설정 값, 쿠키 제거
+    private void clearAuthenticationAttribute(HttpServletRequest request,
+                                              HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        authorizationRequestBasedOnCookieRepository.removeAuthorizationRequestCookie(request, response);
+    }
+
+    // 4. 엑세스 토큰을 패스에 추가
+    private String getTargetUrl(String token) {
+        return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
+                .queryParam("token", token)
+                .build()
+                .toUriString();
+    }
+
+}
